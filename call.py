e7f2f0443dd8f328213618082e8e1b24845bc282
Raul Migliari <raulmigliari5@gmail.com>
2025-04-15 13:38:59 -0300

    Description of call states

--- call.py
+++ call.py
@@ -2,3 +2,13 @@ class Call:
     def __init__(self, id):
         self.id = id
         self.state = 'waiting' # 'waiting', 'ringing', 'answered', 'finished', 'missed'
+
+        '''
+        
+        waiting: waiting in queue
+        ringing: being handed over to an operator
+        answered: answered by an operator
+        finished: ended normally
+        missed: hang up before answering
+        
+        '''
